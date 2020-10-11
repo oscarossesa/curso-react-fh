772301125f0d7b483b28b88f6aea8c1fdc39b3e4
Oscar Osses <oscarossesa@gmail.com>
2020-10-10 22:31:49 -0300

    docs agregado.

---
+++ docs/precache-manifest.99b9f4a0d9fd076497f07cb13a14f324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e35d194313300b70e51a4ea014e043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96000ff62a5fa69f8e19",
+    "url": "/static/css/main.602b2474.chunk.css"
+  },
+  {
+    "revision": "1fed5fcffd77345a81b6",
+    "url": "/static/js/2.8f3e0345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f3e0345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96000ff62a5fa69f8e19",
+    "url": "/static/js/main.a37671c0.chunk.js"
+  },
+  {
+    "revision": "b57b4c68968d7e3520e2",
+    "url": "/static/js/runtime-main.3a5e2f83.js"
+  }
+]);
