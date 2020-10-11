370738bfbb89fc78fa02c58f15ca0877620455f0
Oscar Osses <oscarossesa@gmail.com>
2020-10-10 22:50:50 -0300

    agrego https a petición.

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getFetchGifts = async (category) => {
   const apiKey = 'wLdpjbX2u9soMZ5o5BOgZQ0onDEe8hfk';
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
   const resp = await fetch(url)
   const { data } = await resp.json()
 
