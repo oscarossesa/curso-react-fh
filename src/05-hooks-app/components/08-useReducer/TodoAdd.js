46cba6acca064e5e2770632dc17a4f08c4ca9be3
Oscar Osses <oscarossesa@gmail.com>
2020-10-12 19:15:50 -0300

    Optimización de TodoApp.

---
+++ src/05-hooks-app/components/08-useReducer/TodoAdd.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useForm } from '../../hooks/useForm'
+
+const TodoAdd = ({ handleAddTodo }) => {
+
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: '',
+  })
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (description.trim().length <= 0) return 
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false
+    } 
+
+    handleAddTodo(newTodo)
+    reset()
+  }
+
+  return (
+    <>
+      <h4>Agregar TODO</h4>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          name="description"
+          placeholder="Aprender..."
+          autoComplete="off"
+          value={description}
+          onChange={handleInputChange}
+        />
+        <button
+          type="submit"
+          className="btn btn-outline-primary mt-1 btn-block"
+        >Agregar</button>
+      </form>
+    </>
+  )
+}
+
+export default TodoAdd
--- src/05-hooks-app/components/08-useReducer/TodoApp.js
+++ src/05-hooks-app/components/08-useReducer/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from './todoReducer'
-import { useForm } from '../../hooks/useForm'
+import TodoList from './TodoList'
+import TodoAdd from './TodoAdd'
 
 import './styles.css'
 
@@ -17,41 +18,30 @@ const init = () => {
 const TodoApp = () => {
 
   const [todos, dispatch] = useReducer(todoReducer, initialState, init)
-  const [{ description }, handleInputChange, reset] = useForm({ 
-    description: '', 
-  })
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    if (description.trim().length <= 0) return 
-
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false
-    }
-
-    const action = {
+  const handleAddTodo = (newTodo) => {
+    dispatch({
       type: 'ADD',
       payload: newTodo
-    }
-
-    dispatch(action)
-    reset()
+    })
   }
 
   const handleDelete = (todoId) => {
-    const action = {
+    dispatch({
       type: 'DELETE',
       payload: todoId
-    }
+    })
+  }
 
-    dispatch(action)
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: 'TOGGLE',
+      payload: todoId
+    })
   }
 
   return (
@@ -60,44 +50,15 @@ const TodoApp = () => {
       <hr />
       <div className="row">
         <div className="col-7">
-          <ul className="list-group list-group-flush">
-            {
-              todos.map((todo, i) => (
-                <li 
-                  key={todo.id}
-                  className="list-group-item"
-                >
-                  <p className="text-center">
-                    {i + 1}. {todo.desc}
-                  </p>
-                  <button 
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(todo.id)}
-                  >Borrar</button>
-                </li>
-              ))
-            }
-          </ul>
+          <TodoList
+            todos={todos}
+            handleToggle={handleToggle}
+            handleDelete={handleDelete} />
         </div>
         <div className="col-5">
-          <h4>Agregar TODO</h4>
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="form-control"
-              name="description"
-              placeholder="Aprender..."
-              autoComplete="off"
-              value={description}
-              onChange={handleInputChange}       
-            />
-            <button
-              type="submit"
-              className="btn btn-outline-primary mt-1 btn-block"
-            >Agregar</button>
-          </form>
-        </div>
+          <TodoAdd 
+            handleAddTodo={handleAddTodo} />
+        </div> 
       </div>
     </div>
   )
