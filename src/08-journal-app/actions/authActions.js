804abf7ddd894a16cdb91094648395e7c935c867
Oscar Osses <oscarossesa@gmail.com>
2020-10-14 18:44:54 -0300

    Mantener el estado de la autenticación al recargar.

--- src/08-journal-app/actions/authActions.js
+++ src/08-journal-app/actions/authActions.js
@@ -1,22 +1,26 @@
 import { types } from "../types/types"
 import { firebase, googleAuthProvider } from '../firebase/firebase-config'
+import { finishLoading, startLoading } from "./uiActions"
 
 export const startLoginEmailPassword = (email, password) => {
   return (dispatch) => {
+    dispatch(startLoading())
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(({ user }) => {
-      dispatch(login(user.uid, user.displayName))
-    })
-    .catch((e) => {
-      console.log(e)
-    })    
+      .then(({ user }) => {
+        dispatch(login(user.uid, user.displayName))
+        dispatch(finishLoading())
+      })
+      .catch((e) => {
+        dispatch(finishLoading())
+        console.log(e)
+      })
   }
 }
 
 export const startRegisterWithEmailPasswordName = (email, password, name) => {
   return (dispatch) => {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then( async ({ user }) => {
+      .then(async ({ user }) => {
         await user.updateProfile({ displayName: name })
         dispatch(login(user.uid, user.displayName))
       })
@@ -37,7 +41,7 @@ export const startGoogleLogin = () => {
   }
 }
 
-export const login = (uid, displayName) => ({  
+export const login = (uid, displayName) => ({
   type: types.login,
   payload: {
     uid,
--- src/08-journal-app/components/auth/LoginScreen.js
+++ src/08-journal-app/components/auth/LoginScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from '../../hooks/useForm'
 import { startGoogleLogin, startLoginEmailPassword } from '../../actions/authActions'
 
 const LoginScreen = () => {
 
   const dispatch = useDispatch()
+  const { loading } = useSelector(state => state.ui)
 
   const [ formValues, handleInputChange ] = useForm({
     email: 'oscar',
@@ -49,6 +50,7 @@ const LoginScreen = () => {
         <button
           type="submit"
           className="btn btn-primary btn-block"
+          disabled={loading}
         >
           Login
         </button>
